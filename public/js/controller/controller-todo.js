42ee98b71f978ac8899984e0cb32c8cdaa588987
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-12 12:19:59 -0400

    Agregando logica de eliminado de elemento y logica de agregado en primer position

--- public/js/controller/controller-todo.js
+++ public/js/controller/controller-todo.js
@@ -1,6 +1,23 @@
 app.controller("controller-todo", ["$scope", function($scope){
 
     $scope.listItems = ["comer a las 11am", "Ir a correr"];
+    $scope.active = 0;
+    $scope.addList = function(l){
+        if ( l === ""){
+            floatNotificationError("Rellene con algo")
+        }
+        else{
+            $scope.listItems.unshift(l);
+        }
 
+    };
+
+    $scope.deleteList = function(x){
+       for(let i=0;i<$scope.listItems.length; i++){
+           if($scope.listItems[i] === x){
+                $scope.listItems[i] = 'deleted'
+           }
+       }
+    }
 
 }]);
